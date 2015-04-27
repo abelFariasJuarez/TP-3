2eedac50dc4628f0834c213d4b9e24c2ad254c82
Abel Farias Juarez <fari2207@gmail.com>
2015-04-26 22:02:44 -0300

    refactor usando const

---
+++ TablaCelAFahr.c
@@ -0,0 +1,22 @@
+/***************************************************
+Fahrenheit (°F) = [ Celsius (°C) x 1.8 ] + 32
+usando "const"
+Abel Cesar, Farias Juarez
+20150413
+*****************************************************/
+
+#include <stdio.h> 
+
+int main(void){
+	const int tablaDesde = -25;
+	const int tablaHasta = 75;
+	const float factorConversion = 1.8;
+	const int ajusteConversion = 32;
+	
+	printf("\t%s -->\t%s\n","Celsius", "Fahrenheit");
+	for(int cel= tablaDesde ; cel < tablaHasta; cel++){
+		/*puts("-_-_- HOLA MUNDO -_-_-\n");*/
+      	
+		  printf("\t%03d -->\t%04.2f\n",cel, ( (cel * factorConversion) + ajusteConversion) );
+    }	
+}
